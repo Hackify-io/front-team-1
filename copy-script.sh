00b486554ddf354c173f3ee95feeb1a42ea92611
PaperMonoid <daniel.santiago.aguila.torres@gmail.com>
2020-01-18 14:49:38 -0800

    fix: path to back-end project

--- copy-script.sh
+++ copy-script.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 
 # Copy JavaScript files
-if [ -d ../back-team-1/static/js ]
+if [ -d ../back-team-1/where2med/static/js ]
 then
-    rm -Rf ../back-team-1/static/js/*
+    rm -Rf ../back-team-1/where2med/static/js/*
 else
-    mkdir -p ../back-team-1/static/js
+    mkdir -p ../back-team-1/where2med/static/js
 fi
 
-cp dist/*.bundle.js ../back-team-1/static/js
-cp node_modules/react/umd/react.production.min.js ../back-team-1/static/js
-cp node_modules/react-dom/umd/react-dom.production.min.js ../back-team-1/static/js
+cp dist/*.bundle.js ../back-team-1/where2med/static/js
+cp node_modules/react/umd/react.production.min.js ../back-team-1/where2med/static/js
+cp node_modules/react-dom/umd/react-dom.production.min.js ../back-team-1/where2med/static/js
 
 
 # Copty CSS files
-if [ -d ../back-team-1/static/css ]
+if [ -d ../back-team-1/where2med/static/css ]
 then
-    rm -Rf ../back-team-1/static/css/*
+    rm -Rf ../back-team-1/where2med/static/css/*
 else
-    mkdir -p ../back-team-1/static/css
+    mkdir -p ../back-team-1/where2med/static/css
 fi
 
-cp node_modules/bootstrap/dist/css/bootstrap.min.css ../back-team-1/static/css
+cp node_modules/bootstrap/dist/css/bootstrap.min.css ../back-team-1/where2med/static/css
 
 # Copy django templates
-if [ -d ../back-team-1/templates ]
+if [ -d ../back-team-1/where2med/templates ]
 then
-    rm -Rf ../back-team-1/templates/*
+    rm -Rf ../back-team-1/where2med/templates/*
 else
-    mkdir ../back-team-1/templates
+    mkdir ../back-team-1/where2med/templates
 fi
 
-cp templates/base.html ../back-team-1/templates
-cp -R templates/where2med_auth ../back-team-1/templates
-cp -R templates/home_page ../back-team-1/templates
-cp -R templates/admin_dashboard ../back-team-1/templates
-cp -R templates/scheduling ../back-team-1/templates
+cp templates/base.html ../back-team-1/where2med/templates
+cp -R templates/where2med_auth ../back-team-1/where2med/templates
+cp -R templates/home_page ../back-team-1/where2med/templates
+cp -R templates/admin_dashboard ../back-team-1/where2med/templates
+cp -R templates/scheduling ../back-team-1/where2med/templates
